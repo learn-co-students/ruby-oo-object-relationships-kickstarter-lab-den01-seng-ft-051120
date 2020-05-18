47199abc540d8b1029a1149ee9ad1d1a0ec22116
github username <github email address>
2020-05-18 11:31:09 -0600

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class Backer
+  attr_reader :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def back_project(project)
+    ProjectBacker.new project, self
+  end
+
+  def backed_projects
+    ProjectBacker.all.select { |project| project.backer == self }.map(&:project)
+  end
+end
---
+++ lib/project_backer.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class ProjectBacker
+  attr_reader :project, :backer
+  @@all = []
+
+  def initialize(project, backer)
+    @project = project
+    @backer = backer
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
